79c74f1668959ee06c79e75749932ccfa38d93bd
brebanv <brebanvlad@gmail.com>
2017-04-03 21:36:43 +0300

    Simple list of users

--- index.php
+++ index.php
@@ -1,26 +1,34 @@
 <!DOCTYPE html>
 <html>
+<head>
+    <title>vmiProject</title>
+</head>
 <body>
-<title>vmiProject</title>
 
-<?php
+<center>
+    <table>
+        <tr><th>id</th><th>First Name</th><th>Last Name</th><th>Email</th></tr>
+        <?php
 
-$servername = "127.0.0.1";
-$username = "root";
-$password = "";
-$dbname = "users";
-// Create connection
-//$conn = new mysqli($servername, $username, $password);
-$conn = new mysqli($servername, $username, $password, $dbname);
-mysqli_select_db($conn, $db_name);
+        $conn = new mysqli("127.0.0.1", "root", "", "users");
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        echo "Connected successfully";
 
-?>
+        $query = "SELECT * FROM userInfo";
+        $result = mysqli_query($conn,$query);
+
+        while($row = mysqli_fetch_array($result)){
+            echo "<tr><td>".$row["idUser"]."</td><td>".$row["firstName"]."</td><td>".$row["lastName"]."</td><td>".$row["email"]."</td></tr>";
+        }
+
+        $conn->close();
+        ?>
+    </table>
+</center>
 
 </body>
 </html>
