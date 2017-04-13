13c71fa2b7758abe454cffa4c658a3443707c911
madalinaghiman <madalina.ghiman@yahoo.com>
2017-04-14 01:29:35 +0300

    Update homePage.php

--- homePage.php
+++ homePage.php
@@ -7,11 +7,56 @@
  */
 ?>
 <html>
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+<link rel='stylesheet' type='text/css' href='//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>
 <body>
-<form action="../logout.php">
-    <button align="right">LOG OUTtt </button>
-</form>
-<img src="forum.jpg" align="top" width="300" height="160" border="4" hspace="10" vspace="7"></img>
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+
+            </button>
+            <a class="navbar-brand" href="#">Adolescenti</a>
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+
+            </button>
+            <a class="navbar-brand" href="#">Sport</a>
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+
+            </button>
+            <a class="navbar-brand" href="#">Arta</a>
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+
+            </button>
+            <a class="navbar-brand" href="#">Tehnologie</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+            <form class="navbar-form navbar-left">
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Search">
+                </div>
+                <button type="submit" class="btn btn-default">Submit</button>
+            </form>
+            <ul class="nav navbar-nav navbar-right">
+
+               <li> <form action="../logout.php">
+                       <button align="right">LOG OUTtt </button></li>
+
+                </li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+</nav>
+
+<img  src="forum.jpg" align=center width="300" height="160" border="4" hspace="10" vspace="7"></img>
 
 <body bgcolor="#F5F5F5">
 <div class="container">
