b4d2eb108d98f4ec8aca33c450a2a8fb0c5a59ff
madalinaghiman <madalina.ghiman@yahoo.com>
2017-04-14 00:24:28 +0300

    Update homePage.php

--- homePage.php
+++ homePage.php
@@ -5,10 +5,22 @@
  * Date: 04.04.2017
  * Time: 21:36
  */
-echo "Home page!";
 ?>
 <html>
-    <form action="logout.php">
-        <button>LOGOUT</button>
-    </form>
+<body>
+<form action="../logout.php">
+    <button align="right">LOG OUTtt </button>
+</form>
+<img src="forum.jpg" align="top" width="300" height="160" border="4" hspace="10" vspace="7"></img>
+
+<body bgcolor="#F5F5F5">
+<div class="container">
+        <label><b>Pune o intrebare</b></label> <br>
+        <input type="text" placeholder="Add question" name="question" required>     <button>Adauga</button>
+</div>
+
+
+</form>
+
+</body>
 </html>
