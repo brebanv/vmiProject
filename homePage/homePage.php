d405885905fee049022625d5e9c37c2a384cb96b
brebanv <brebanvlad@gmail.com>
2017-04-25 22:53:02 +0300

    Add questions logic

--- homePage/homePage.php
+++ homePage/homePage.php
@@ -1,4 +1,5 @@
 <html>
+<label>Home Page!</label>
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 <link rel='stylesheet' type='text/css' href='//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>
 <body>
