ca01023058ac5875c7ba96df140c2ce233a2089c
madalinaghiman <madalina.ghiman@yahoo.com>
2017-05-17 00:35:36 +0300

    Update insert_question_db.php

--- insert_question_db.php
+++ insert_question_db.php
@@ -5,9 +5,9 @@ require_once('../connect_db.php');
 parse_str(file_get_contents("php://input"), $_POST);
 
 $question = $_POST['question'];
+$category= $_POST['category'];
 
-
-$sql = "SELECT question FROM questions WHERE question= '$question'";
+$sql = "SELECT question,category FROM questions WHERE question= '$question' AND category='$category'";
 $result = mysqli_query($conn,$sql);
 $questionCheck = mysqli_num_rows($result);
 
@@ -15,24 +15,24 @@ if ($questionCheck > 0) {
     header("Location: homePage.php?error=question");
     exit();
 } else {
-    $sql = "INSERT INTO questions (question) VALUES ('$question')";
+    $sql = "INSERT INTO questions (question,category) VALUES ('$question','$category')";
     $result = $conn->query($sql);
 
     $sql = "SELECT * FROM questions";
     if($result = mysqli_query($conn, $sql)){
-            if(mysqli_num_rows($result) > 0){
-                    while($row = mysqli_fetch_array($result)){
-                            echo   $row['id'] . "</td>";
-             echo   $row['question'] . "</td>";
-             echo "     <br />";
-         }
-         // Free result set
-         mysqli_free_result($result);
-     } else{
-                    echo "No records matching your query were found.";
-     }
- } else{
-            echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
- }
+        if(mysqli_num_rows($result) > 0){
+            while($row = mysqli_fetch_array($result)){
+                echo   $row['id'] . "</td>";
+                echo   $row['question'] . "</td>";
+                echo "     <br />";
+            }
+            // Free result set
+            mysqli_free_result($result);
+        } else{
+            echo "No records matching your query were found.";
+        }
+    } else{
+        echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
+    }
 }
 
