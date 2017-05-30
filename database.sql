3f6e05a403a7b8c43275a154d2843bd7d7ad2236
brebanv <brebanvlad@gmail.com>
2017-05-30 22:44:00 +0300

    DB changes

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 29 Mai 2017 la 19:34
+-- Generation Time: 30 Mai 2017 la 21:43
 -- Versiune server: 10.1.21-MariaDB
 -- PHP Version: 5.6.30
 
@@ -30,24 +30,21 @@ CREATE TABLE `answers` (
   `answerId` int(11) NOT NULL,
   `questionId` int(11) NOT NULL,
   `answer` varchar(255) DEFAULT NULL,
-  `currDate` datetime DEFAULT NULL
+  `currDate` datetime DEFAULT NULL,
+  `userID` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Salvarea datelor din tabel `answers`
 --
 
-INSERT INTO `answers` (`answerId`, `questionId`, `answer`, `currDate`) VALUES
-(1, 37, 'dsad', '2017-05-27 17:16:25'),
-(2, 37, 'dsad', '2017-05-27 17:16:43'),
-(3, 37, 'Nimic', '2017-05-27 17:39:38'),
-(4, 37, 'ce?', '2017-05-27 17:40:50'),
-(5, 37, 'dsad', '2017-05-27 17:41:15'),
-(6, 37, 'Bine, tu?', '2017-05-27 17:41:38'),
-(7, 38, 'Bine', '2017-05-27 17:42:53'),
-(8, 43, 'Daaaa', '2017-05-28 11:28:04'),
-(9, 42, 'Da', '2017-05-28 11:28:26'),
-(10, 43, 'Nimic', '2017-05-28 11:29:17');
+INSERT INTO `answers` (`answerId`, `questionId`, `answer`, `currDate`, `userID`) VALUES
+(14, 69, 'religie?', '2017-05-30 22:07:44', 9),
+(15, 69, 'religie?', '2017-05-30 22:08:20', 9),
+(16, 70, 'ggg', '2017-05-30 22:15:55', 9),
+(17, 70, 'gggdddd', '2017-05-30 22:16:13', 9),
+(18, 78, 'fff', '2017-05-30 22:24:50', 9),
+(19, 79, 'ddd', '2017-05-30 22:26:44', 10);
 
 -- --------------------------------------------------------
 
@@ -59,17 +56,27 @@ CREATE TABLE `questions` (
   `id` int(11) NOT NULL,
   `question` varchar(255) NOT NULL,
   `category` varchar(255) DEFAULT NULL,
-  `currDate` datetime DEFAULT NULL
+  `currDate` datetime DEFAULT NULL,
+  `userID` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Salvarea datelor din tabel `questions`
 --
 
-INSERT INTO `questions` (`id`, `question`, `category`, `currDate`) VALUES
-(38, 'Ce faci?', 'Religie', '2017-05-27 14:59:26'),
-(43, 'da?', 'Diverse', '2017-05-27 18:48:02'),
-(46, 'dsad', '0', '2017-05-28 11:58:37');
+INSERT INTO `questions` (`id`, `question`, `category`, `currDate`, `userID`) VALUES
+(69, 'Religie', 'Religie', '2017-05-30 21:27:29', 10),
+(70, 'a', 'Sport', '2017-05-30 21:30:40', 10),
+(71, 'aaa', 'Scoala', '2017-05-30 21:31:16', 10),
+(72, 'dasdad', 'Religie', '2017-05-30 21:31:44', 10),
+(73, 'aaa', 'Tehnologie', '2017-05-30 21:32:35', 9),
+(74, 'aaaff', 'Tehnologie', '2017-05-30 21:40:42', 9),
+(75, 'aaaaa', 'Religie', '2017-05-30 21:46:30', 9),
+(76, 'aaaaaa', 'Religie', '2017-05-30 21:49:44', 9),
+(77, 'Tttttt', 'Scoala', '2017-05-30 21:53:52', 9),
+(78, 'hhhhh', 'Sport', '2017-05-30 22:06:08', 9),
+(79, 'Intrebare', 'Sport', '2017-05-30 22:26:36', 10),
+(80, 'Intrebare', 'Diverse', '2017-05-30 22:34:52', 10);
 
 -- --------------------------------------------------------
 
@@ -126,12 +133,12 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `answers`
 --
 ALTER TABLE `answers`
-  MODIFY `answerId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `answerId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 --
 -- AUTO_INCREMENT for table `questions`
 --
 ALTER TABLE `questions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=81;
 --
 -- AUTO_INCREMENT for table `user`
 --
